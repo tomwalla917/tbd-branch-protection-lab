0211c406438d4569712a086c333458aafbb94c95
Tom Wallace <tywalla@gmail.com>
2026-03-03 20:39:26 -0500

    Delete Starter directory

--- Starter/src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-// Use environment variable when available, otherwise use local default.
-const port = process.env.PORT || 3000;
-
-// Start server.
-app.listen(port, () => {
-  console.log(`API running at http://localhost:${port}`);
-});
