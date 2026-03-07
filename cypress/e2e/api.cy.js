af068295b90294e90a3608b81a6990885720c55c
tomwalla917 <tywalla@gmail.com>
2026-03-07 13:54:09 -0500

    check push

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -2,6 +2,7 @@ describe('GHCR lab starter API', () => {
   it('GET / returns welcome message', () => {
     cy.request('/').then((response) => {
       expect(response.status).to.eq(200);
+      expect(response.body.status).to.eq('ok');
       expect(response.body.message).to.eq('Welcome to the GHCR lab starter API');
     });
   });
